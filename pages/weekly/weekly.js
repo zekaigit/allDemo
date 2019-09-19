f98bc75ab87c61dca96f3983c71749e37a087e02
unknown <unknown@example.com>
2019-09-19 21:56:19 +0800

    0919

---
+++ pages/weekly/weekly.js
@@ -0,0 +1,16 @@
+Page({
+  data:{
+    thisWeekMovie:[{
+      name: "教父",
+      comment: "最精彩的剧本， 最真实的黑帮电影",
+      imagePath: "/image/diannao.jpg",
+      isHighRecommended:false
+    },
+    {
+      name: "杀手",
+      comment: "最精彩的剧本， 最真实的黑帮电影",
+      imagePath: "/image/diannao.jpg",
+      isHighRecommended: false    }
+  
+  ]
+})
